b5d7e4dda17d1bf8c0dd138a3fe37a6bf6dfe53b
Addie Mongelli <addie.mongelli@stash.com>
2023-07-20 20:33:18 -0400

    webcrawler in js

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const { crawlPage } = require(`./crawl.js`)
+const { crawlPage } = require('./crawl.js')
+const { printReport } = require('./report.js')
 
-function main() {
-    if (process.argv.length < 3) {
-        console.log("no website provided")
-        process.exit(1)
-    }
+async function main(){
+  if (process.argv.length < 3){
+    console.log('no website provided')
+  }
+  if (process.argv.length > 3){
+    console.log('too many arguments provided')
+  }
 
-    if (process.argv.length < 3) {
-        console.log("too many command line args")
-        process.exit(1)
-    }
+  const baseURL = process.argv[2]
 
-    const baseURL = process.argv[2]
+  console.log(`starting crawl of: ${baseURL}...`)
 
-    console.log(`starting crawl of ${baseURL}`)
-    crawlPage(baseURL) 
+  const pages = await crawlPage(baseURL, baseURL, {})
+
+  printReport(pages)
 }
 
 main()
